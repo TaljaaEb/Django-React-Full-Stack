775195d5041ee798745ada6b4fbf03080d3b2681
TaljaaEb <138305368+TaljaaEb@users.noreply.github.com>
2024-03-28 15:11:10 +0200

    Delete pre-prod.py

--- pre-prod.py
+++
@@ -1,5 +0,0 @@
-class dev.U188156415.runnerz.aByte$$Lambda$882/0x0000000800cd7578
-
-http://backend-829871858:8000/
-
-https://eb6e4d74-3c75-40e0-9681-84f71ad31c4a-dev.e1-us-east-azure.choreoapis.dev/djangoreacttutorial/backend/rest-api-be2/v1.0
